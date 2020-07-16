fb3c0d0efeb65f2d8bc75ccdb5a3530f0921f4c2
hello-lwz <hello-lwz@163.com>
2020-07-16 14:32:52 +0800

    每日一题与哈希表部分题

---
+++ all_topic/esay_topic/1160. 拼写单词.py
@@ -0,0 +1,67 @@
+#!/usr/local/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@author: 
+@file: 1160. 拼写单词.py
+@time: 2020/7/15 17:14
+@desc: 
+"""
+from typing import List
+"""
+给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
+假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
+注意：每次拼写（指拼写词汇表中的一个单词）时，chars 中的每个字母都只能用一次。
+返回词汇表 words 中你掌握的所有单词的 长度之和。
+
+ 
+
+示例 1：
+
+输入：words = ["cat","bt","hat","tree"], chars = "atach"
+输出：6
+解释： 
+可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
+示例 2：
+
+输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+输出：10
+解释：
+可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+ 
+
+提示：
+
+1 <= words.length <= 1000
+1 <= words[i].length, chars.length <= 100
+所有字符串中都仅包含小写英文字母
+"""
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        # from collections import Counter
+        # result = 0
+        # chars_count = Counter(chars)
+        # for word in words:
+        #     word_count = Counter(word)
+        #     flag = False
+        #     for k, v in word_count.items():
+        #         if k not in chars_count.keys() or word_count[k] > chars_count[k]:
+        #             flag = True
+        #             break
+        #     if not flag:
+        #         result += len(word)
+        # return result
+
+        ans = 0
+        for word in words:
+            for char in word:
+                if word.count(char) > chars.count(char):
+                    break
+            else:
+                ans += len(word)
+        return ans
+
+a = Solution().countCharacters(["cat","bt","hat","tree"], "atach")
+b = Solution().countCharacters(["hello","world","leetcode"], "welldonehoneyr")
+print(a)
+print(b)
