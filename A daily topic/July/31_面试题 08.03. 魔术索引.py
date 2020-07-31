2c5786a8c020c4dd7d9efb8f08f33adfb35e65d0
hello-lwz <hello-lwz@163.com>
2020-07-31 10:14:03 +0800

    每日一题

---
+++ A daily topic/July/31_面试题 08.03. 魔术索引.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@author: 
+@file: 面试题 08.03. 魔术索引.py
+@time: 2020/7/31 09:03
+@desc: 
+"""
+from typing import List
+"""
+魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+
+示例1:
+
+ 输入：nums = [0, 2, 3, 4, 5]
+ 输出：0
+ 说明: 0下标的元素为0
+示例2:
+
+ 输入：nums = [1, 1, 1]
+ 输出：1
+提示:
+
+nums长度在[1, 1000000]之间
+"""
+
+class Solution:
+    def findMagicIndex(self, nums: List[int]) -> int:
+        length = len(nums)
+        i = 0
+        while i < length:
+            if nums[i] == i:
+                return i
+            i = max(nums[i], i+1)
+        return -1
+
+
